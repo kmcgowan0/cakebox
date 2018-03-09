c1ae5bdaad5fcdec7a965540dc237f4f5605efea
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-03-09 11:14:23 +0000

    Messages functionality
    #102 recipient id should be set automatically
    #98 sent time should be current date and time
    #99 sender id should be assigned to current user
    #101 sender id should be set automatically

---
+++ mycake3.app/src/Controller/MessagesController.php
@@ -0,0 +1,130 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Messages Controller
+ *
+ * @property \App\Model\Table\MessagesTable $Messages
+ *
+ * @method \App\Model\Entity\Message[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class MessagesController extends AppController
+{
+
+    public $uses = array('Messages', 'Users');
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+//        $this->paginate = [
+//            'contain' => ['Senders', 'Recipients']
+//        ];
+        $messages = $this->paginate($this->Messages);
+
+        $this->set(compact('messages'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Message id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $message = $this->Messages->get($id);
+
+        $this->set('message', $message);
+    }
+
+    public function inbox() {
+        $messages = $this->Messages->find('all', array(
+            'conditions' => array(
+                'recipient' => $this->Auth->user('id')
+            )
+        ));
+        $this->set('messages', $messages);
+    }
+
+    public function outbox() {
+        $messages = $this->Messages->find('all', array(
+            'conditions' => array(
+                'sender' => $this->Auth->user('id')
+            )
+        ));
+        $this->set('messages', $messages);
+    }
+
+    public function compose($recipient = null) {
+        $message = $this->Messages->newEntity();
+        if ($this->request->is('post')) {
+
+            $message_data = $this->request->getData();
+            $message_data['sender'] = $this->Auth->user('id');
+            $message_data['recipient'] = $recipient;
+            $message_data['sent'] = date('Y-m-d h:i');
+            $message = $this->Messages->patchEntity($message, $message_data);
+            if ($this->Messages->save($message)) {
+                $this->Flash->success(__('Message sent'));
+                return $this->redirect(['action' => 'outbox']);
+            }
+            $this->Flash->error(__('The message could not be sent. Please, try again.'));
+
+        }
+        $this->loadModel('Users');
+        $recipients = $this->Users->find('list', ['limit' => 200]);
+        $this->set(compact('message', 'recipients'));
+
+    }
+    /**
+     * Edit method
+     *
+     * @param string|null $id Message id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $message = $this->Messages->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $message = $this->Messages->patchEntity($message, $this->request->getData());
+            if ($this->Messages->save($message)) {
+                $this->Flash->success(__('The message has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The message could not be saved. Please, try again.'));
+        }
+//        $senders = $this->Messages->Senders->find('list', ['limit' => 200]);
+//        $recipients = $this->Messages->Recipients->find('list', ['limit' => 200]);
+        $this->set(compact('message'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Message id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $message = $this->Messages->get($id);
+        if ($this->Messages->delete($message)) {
+            $this->Flash->success(__('The message has been deleted.'));
+        } else {
+            $this->Flash->error(__('The message could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ mycake3.app/src/Template/Messages/view.ctp
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Message $message
+ */
+?>
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Html->link(__('Edit Message'), ['action' => 'edit', $message->id]) ?> </li>
+        <li><?= $this->Form->postLink(__('Delete Message'), ['action' => 'delete', $message->id], ['confirm' => __('Are you sure you want to delete # {0}?', $message->id)]) ?> </li>
+        <li><?= $this->Html->link(__('List Messages'), ['action' => 'index']) ?> </li>
+        <li><?= $this->Html->link(__('New Message'), ['action' => 'add']) ?> </li>
+    </ul>
+</nav>
+<div class="messages view large-9 medium-8 columns content">
+    <h3><?= h($message->id) ?></h3>
+    <table class="vertical-table">
+        <tr>
+            <th scope="row"><?= __('Body') ?></th>
+            <td><?= h($message->body) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Id') ?></th>
+            <td><?= $this->Number->format($message->id) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Sender') ?></th>
+            <td><?= $this->Number->format($message->sender) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Recipient') ?></th>
+            <td><?= $this->Number->format($message->recipient) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Sent') ?></th>
+            <td><?= h($message->sent) ?></td>
+        </tr>
+    </table>
+</div>
