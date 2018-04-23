ae33d52af68c3ca31e92f9432a52de89d21a68b6
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-04-23 15:06:28 +0100

    trying to fix CORS issue

--- mycake3.app/src/Controller/AppController.php
+++ mycake3.app/src/Controller/AppController.php
@@ -62,5 +62,10 @@ class AppController extends Controller
     {
         $this->Auth->allow(['index', 'display', 'connectionMessages', 'instantMessages']);
         $this->set('authUser', $this->Auth->user());
+        $this->response->header('Access-Control-Allow-Origin','*');
+        $this->response->header('Access-Control-Allow-Methods','*');
+        $this->response->header('Access-Control-Allow-Headers','X-Requested-With');
+        $this->response->header('Access-Control-Allow-Headers','Content-Type, x-xsrf-token');
+        $this->response->header('Access-Control-Max-Age','172800');
     }
 }
